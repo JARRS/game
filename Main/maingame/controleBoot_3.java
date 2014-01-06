6c3787824387429331f457c23bc3ea315d3b1dcf
DhirajSanchit <Dhirajsanchit@gmail.com>
2014-01-06 21:56:24 +0100

    Classes voor verschillende boten voor Controlecentrum + images

---
+++ Main/maingame/controleBoot_3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class controleBoot_3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class controleBoot_3 extends controleHavenObjecten
+{
+    /**
+     * Act - do whatever the controleBoot_3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
