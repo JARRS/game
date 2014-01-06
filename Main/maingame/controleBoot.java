049b8b996cf4e8d3159703a9cc4fa11517c0fcdd
Melloww <RobinSiep1@gmail.com>
2014-01-06 21:40:52 +0100

    boten

--- Main/maingame/controleBoot.java
+++ Main/maingame/controleBoot.java
@@ -12,8 +12,42 @@ public class controleBoot extends controleHavenObjecten
      * Act - do whatever the controleBoot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    boolean geklikt = false;
+    boolean varen = true;
+    int mx = 0;
+    int my = 0;
     public void act() 
     {
-        // Add your action code here.
+       if (Greenfoot.mouseClicked(this) == true && geklikt == false){
+            geklikt = true;
+        }
+       else if (Greenfoot.mouseClicked(this) == true && geklikt == true){
+           varen = true;
+           
+           MouseInfo mouse = Greenfoot.getMouseInfo(); 
+            if(mouse!=null){  
+               mx = mouse.getX();  
+               my = mouse.getY();  
+            }  
+            geklikt = false;
+            
+        }
+       
+       if(varen == true){
+           
+           
+           
+           //aan het einde van de act
+           if(getX() == mx && getY() == my){
+               varen = false;
+            }
+        }
     }    
+    
+    public void varen(){
+        
+        
+        
+        
+    }
 }
