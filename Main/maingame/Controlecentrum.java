2c2c3b883592e2fa74e1b3da605cf879ba5e6922
DhirajSanchit <Dhirajsanchit@gmail.com>
2014-01-06 18:14:48 +0100

    Ff de haven grotes gedaan, als je best vindt ga ik verder ben nu eten

--- Main/maingame/Controlecentrum.java
+++ Main/maingame/Controlecentrum.java
@@ -14,7 +14,45 @@ public class Controlecentrum extends Minigames
      * 
      */
     public Controlecentrum()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    {
+        fill();
+        taskbar();
+       
+    }
+    
+    
+    /* maakt controlehaven  */
+    void fill(){
+        for (int y = 38; y < 45; y++){
+            for(int x = 0; x < 52; x++){
+                 addObject(new controleHaven(), x, y);
+               
+                }
+            }
+        /* "linker" stuk */
+        for (int y = 20; y < 40; y++){
+            for(int x = 0; x < 4; x++){
+                addObject(new controleHaven(), x, y);
+
+            }
+        }
+        
+        /* "middel" stuk */
+        for (int y = 20; y < 40; y++){
+            for(int x = 22; x < 27; x++){
+               addObject(new controleHaven(), x, y);
+
+            }
+        }
+        
+        
+        /* "rechter" stuk */
+        
+        for (int y = 20; y < 40; y++){
+            for(int x = 46; x < 52; x++){
+                addObject(new controleHaven(), x, y);
+
+            }
+        }
     }
 }
