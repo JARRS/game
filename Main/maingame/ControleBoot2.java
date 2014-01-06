0ad606bafb8d76471a8627ccf78f24d44840b6ba
Melloww <RobinSiep1@gmail.com>
2014-01-06 22:08:03 +0100

    controleboten

---
+++ Main/maingame/ControleBoot2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ControleBoot2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ControleBoot2 extends controleBoot
+{
+    /**
+     * Act - do whatever the ControleBoot2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
